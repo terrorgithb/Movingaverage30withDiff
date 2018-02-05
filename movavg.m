6f5315e4a0f848b481af43b04c8c6b4e8a3ab882
terrorgithb <t101.v151@outlook.com>
2018-02-05 11:02:25 +0530

    Add files via upload

--- movavg.m
+++ movavg.m
@@ -4,32 +4,25 @@ close all;
 %% Create data
 
 % Globals
-window = 7;
+window = 10;
 
 % Filename
-load data.csv;
+load Jan.csv;
 
 % Read TRG data
-year = data(:,1);
-IMDr = data(:,2);
-GKVKr = data(:,3);
+year = Jan(:,1);
+Maxr = Jan(:,2);
+Minr = Jan(:,3);
+data = Minr;
 
-%avg = zeros(length(year),2);
-avg1 = zeros(length(year),2);
+avg = zeros(length(year),2);
 diff = zeros(length(year),2);
 
-%avg(:,1) = smooth(year,IMDr,5,'moving');
-%avg(:,2) = smooth(year,GKVKr,5,'moving');
-avg1(:,1) = filter(ones(1,window)/window, 1, flip(IMDr));
-avg1(:,2) = filter(ones(1,window)/window, 1, flip(GKVKr));
+avg1 = filter(ones(1,window)/window, 1, data);
+%avg1 = Jan(:,4);
 
 %subplot(2,1,1)
-plot(year,flip(avg1(:,1)),'b-',year,flip(avg1(:,2)),'r-',year,IMDr,'b:',year,GKVKr,'r:')
-set(gca,'XLim',[1972 2015])
-legend('IMD avg','GKVK avg','IMD raw','GKVK raw','Location','NW')
-title('Moving avg over 5 years')
+plot(year,data,'b:',year,avg1,'r-');
 
 %subplot(2,1,2)
-%area(year,avg1(:,1)-avg1(:,2),'FaceColor',[0.5 0.9 0.6])
-%set(gca,'XLim',[1972 2015])
-%title('difference')
+%area(year,data-avg1,'FaceColor',[0.5 0.9 0.6]);
