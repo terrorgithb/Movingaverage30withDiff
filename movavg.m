62b3ec6784bca30ea1670aca4dbf20b531805ae2
terrorgithb <t101.v151@outlook.com>
2018-02-05 11:07:56 +0530

    Add files via upload

--- movavg.m
+++ movavg.m
@@ -22,7 +22,15 @@ avg1 = filter(ones(1,window)/window, 1, data);
 %avg1 = Jan(:,4);
 
 %subplot(2,1,1)
-plot(year,data,'b:',year,avg1,'r-');
+plot(year,data,'b:',year,avg1,'r-')
+hold on
+set(gca,'XLim',[1980 2019])
+legend('Min average','Moving average','Location','NW')
+title('Moving avg over 10 years')
+xlabel('Years') % x-axis label
+ylabel('Annual RF') % y-axis label
+hold off
+
 
 %subplot(2,1,2)
 %area(year,data-avg1,'FaceColor',[0.5 0.9 0.6]);
